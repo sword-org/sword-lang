8382f8d81e89695ee69f8388455344e20edf0c1d
Martin Stallman <cn2005nian@163.com>
2015-02-04 11:06:21 +0800

    add file operator

--- src/org/sword/lang/HttpUtils.java
+++ src/org/sword/lang/HttpUtils.java
@@ -5,12 +5,19 @@ package org.sword.lang;
 
 
 
+import java.io.File;
+import java.io.InputStream;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
@@ -33,7 +40,7 @@ public class HttpUtils {
 	 * @return
 	 */
 	public static String post(String url) {
-		return post(url, null);
+		return post(url, "");
 	}
 	
 	/**
@@ -84,6 +91,68 @@ public class HttpUtils {
 		}
 		return null;
 	}
+	
+	/** 发送http post请求
+	 * @param url       url
+	 * @param instream  post数据的字节流
+	 * @return
+	 */
+	public static String post(String url, InputStream instream){
+		try {
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpPost post = new HttpPost(url);
+			if(instream != null){
+				HttpEntity reqEntity = new InputStreamEntity(instream, -1);
+				post.setEntity(reqEntity);
+			}
+			HttpResponse response = httpClient.execute(post);
+			HttpEntity entity = response.getEntity();
+			return entity != null ? EntityUtils.toString(entity) : null;
+		} catch (Exception e) {
+			logger.error("post请求异常，" + e.getMessage() + "\n post url:" + url);
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * 上传文件
+	 * @param url    URL
+	 * @param file   需要上传的文件
+	 * @return
+	 */
+	public static String postFile(String url,File file){
+		return postFile(url, null, file);
+	}
+	
+	/**
+	 * 上传文件
+	 * @param url    URL
+	 * @param name   文件的post参数名称
+	 * @param file   上传的文件
+	 * @return
+	 */
+	public static String postFile(String url,String name,File file){
+		try {
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpPost post = new HttpPost(url);
+			if(file != null){
+				FileBody fileBody = new FileBody(file);
+				MultipartEntity  reqEntity = new MultipartEntity();
+				reqEntity.addPart(name, fileBody);
+				post.setEntity(reqEntity);
+			}
+			HttpResponse response = httpClient.execute(post);
+			HttpEntity entity = response.getEntity();
+			return entity != null ? EntityUtils.toString(entity) : null;
+		} catch (Exception e) {
+			logger.error("post请求异常，" + e.getMessage() + "\n post url:" + url);
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+
 
 	/**
 	 * 发送get请求
@@ -100,6 +169,25 @@ public class HttpUtils {
 			return entity != null ? EntityUtils.toString(entity) : null;
 		} catch (Exception e) {
 			logger.error("get请求异常，" + e.getMessage() + "\n get url:" + url);
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * 下载文件
+	 * @param url   URL
+	 * @return      文件的二进制流，客户端使用outputStream输出为文件
+	 */
+	public static byte[] getFile(String url){
+		try {
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpGet get = new HttpGet(url);
+			HttpResponse response = httpClient.execute(get);
+			HttpEntity resEntity = response.getEntity();
+			return EntityUtils.toByteArray(resEntity);
+		} catch (Exception e) {
+			logger.error("postFile请求异常，" + e.getMessage() + "\n post url:" + url);
 			e.printStackTrace();
 		}
 		return null;
